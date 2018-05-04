994ed5ca95dd1d4bd73439e6f36f12e0066af573
moseslucas <mo@moseslucas.com>
2018-05-05 03:13:53 +0800

    Fixes font issues in Android

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import ContactList from './src/ContactList'
 import ContactView from './src/ContactView'
 import ContactForm from './src/ContactForm'
 import { StackNavigator } from 'react-navigation'
+import Expo from 'expo'
 
 
 const AppStack =  StackNavigator(
@@ -15,9 +16,24 @@ const AppStack =  StackNavigator(
 )
 
 export default class App extends Component {
+  constructor () {
+    super()
+    this.state = { loading: true }
+  }
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+    })
+    this.setState({ loading: false })
+  }
   render () {
-    return (
-      <AppStack/>
-    )
+    if ( !this.state.loading )
+      return (
+        <AppStack/>
+      )
+    else
+      return null
   }
 }
