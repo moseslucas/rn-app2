b5accdadc97aa583317fd6924dd8aa7c69b46650
moseslucas <mo@moseslucas.com>
2018-08-16 23:54:34 +0800

    Basic list and phonecall function

--- App.js
+++ App.js
@@ -1,39 +1,55 @@
-import React, { Component } from 'react'
-import ContactList from './src/ContactList'
-import ContactView from './src/ContactView'
-import ContactForm from './src/ContactForm'
-import { StackNavigator } from 'react-navigation'
-import Expo from 'expo'
+// import React
+import React from 'react';
+// import the components we need from nativebase
+import { Container, Body, Header, Content, List, ListItem, Text, Left, Right, Icon } from 'native-base';
+// import phonecall sa we can call some digitz
+import { phonecall } from 'react-native-communications'
 
+class App extends React.Component {
 
-const AppStack =  StackNavigator(
-  {
-    ContactList: { screen: ContactList },
-    ContactView: { screen: ContactView },
-    ContactForm: { screen: ContactForm }
-  }, 
-  { initialRouteName: 'ContactList' }
-)
-
-export default class App extends Component {
-  constructor () {
-    super()
-    this.state = { loading: true }
+  dial (mobile) {
+    phonecall(mobile, false)
   }
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-    })
-    this.setState({ loading: false })
-  }
-  render () {
-    if ( !this.state.loading )
-      return (
-        <AppStack/>
-      )
-    else
-      return null
+
+  // render here function is required
+  render() {
+    // Our Data Source
+    const contacts = [
+      {name: 'Moses', mobile: '09157569919'},
+      {name: 'Jimi Hendrix', mobile: '09173428288'},
+      {name: 'Batista Bomb', mobile: '09158578928'},
+      {name: 'Hulk Hogan', mobile: '0906928182x'},
+      {name: 'John Cena', mobile: '09759890102'}
+    ]
+
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <List>
+            {
+              // iterate through each contacts
+              contacts.map((contact, i) => {
+                return (
+                  // will render ListItem for each iteration of contacts
+                  // pressing a ListITem will call the dial() function
+                  <ListItem key={i} onPress={ () => {this.dial(contact.mobile)} }>
+                    <Body>
+                      <Text> {contact.name} </Text>
+                      <Text note> {contact.mobile} </Text>
+                    </Body>
+										<Right>
+											<Icon name='arrow-forward' />
+										</Right>
+                  </ListItem>
+                )
+              })
+            }
+          </List>
+        </Content>
+      </Container>
+    );
   }
 }
+
+export default App
